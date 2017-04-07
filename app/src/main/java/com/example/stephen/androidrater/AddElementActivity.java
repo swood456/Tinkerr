025085e550b75b00c092da8f679976041c363ea1
swood456 <crazedsportsfan@gmail.com>
2017-04-06 23:37:05 -0400

    updated add_element_activity to grab data from field on button press. Still only works with finite number of fields

--- app/src/main/java/com/example/stephen/androidrater/AddElementActivity.java
+++ app/src/main/java/com/example/stephen/androidrater/AddElementActivity.java
@@ -2,12 +2,30 @@ package com.example.stephen.androidrater;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RatingBar;
 
 public class AddElementActivity extends AppCompatActivity {
+
+    EditText et_element_name;
+    EditText et_element_description;
+    RatingBar rb_element_rating;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_element);
+
+        et_element_name = (EditText)findViewById(R.id.editText_add_element_name);
+        et_element_description = (EditText)findViewById(R.id.editText_add_element_description);
+        rb_element_rating = (RatingBar)findViewById(R.id.ratingBar_add_element_rating);
+    }
+
+    public void add_element(View view){
+        String element_name = et_element_name.getText().toString();
+        String element_description = et_element_description.getText().toString();
+
+        float element_rating = rb_element_rating.getRating();
     }
 }
