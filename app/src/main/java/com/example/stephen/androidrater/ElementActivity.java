4d9f51636f8423b6c0c23e1e6c9c7975537f9e2a
swood456 <crazedsportsfan@gmail.com>
2017-04-30 20:13:54 -0400

    made element activity useful

--- app/src/main/java/com/example/stephen/androidrater/ElementActivity.java
+++ app/src/main/java/com/example/stephen/androidrater/ElementActivity.java
@@ -2,11 +2,15 @@ package com.example.stephen.androidrater;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 public class ElementActivity extends AppCompatActivity {
 
-    TextView tv_element_name;
+    TextView tv_food_name;
+    TextView tv_restaurant_name;
+    RatingBar rb_rating;
+    TextView tv_description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +19,14 @@ public class ElementActivity extends AppCompatActivity {
 
         DatabaseElement element = this.getIntent().getExtras().getParcelable("element");
 
-        tv_element_name = (TextView) findViewById(R.id.text_element_name);
+        tv_food_name = (TextView) findViewById(R.id.text_element_name);
+        tv_restaurant_name = (TextView) findViewById(R.id.text_element_restaurant);
+        rb_rating = (RatingBar) findViewById(R.id.ratingBar_element_activity_rating);
+        tv_description = (TextView) findViewById(R.id.text_element_description);
 
-        tv_element_name.setText(element.get_food_name());
+        tv_restaurant_name.setText(element.get_restaurant());
+        tv_food_name.setText(element.get_food_name());
+        rb_rating.setRating(element.get_rating());
+        tv_description.setText(element.get_description());
     }
 }
